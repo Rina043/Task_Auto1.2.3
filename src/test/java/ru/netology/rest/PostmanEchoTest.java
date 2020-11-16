2f5d9101fb40cd0cc3cde1dec71e57cdac30cf28
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-11-16 23:45:15 +0400

    Test fixed

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some info"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
